73a89d3bc133f5486b75c67932433c5ff3e1e0aa
shrashti-19 <shrashtidwivedi91@gmail.com>
2026-09-06 23:22:14 +0530

    updated with async/await

--- src/server.js
+++ src/server.js
@@ -1,24 +1,30 @@
 require("dotenv").config();
+
 const express = require("express");
 const crypto = require("crypto");
 const connectDB = require("./config/db");
+const Job = require("./models/Job");
+
 const app = express();
 
 app.use(express.json());
 
-app.post("/jobs", (req, res) => {
+app.post("/jobs", async (req, res) => {
   const { type, payload } = req.body;
 
-  const job = {
-    id: crypto.randomUUID(),
+  const job = new Job({
+    _id: crypto.randomUUID(),
     type,
-    payload,
-    status: "PENDING"
-  };
+    payload
+  });
+
+  await job.save();
 
   res.status(201).json(job);
 });
+
 connectDB();
+
 app.listen(4000, () => {
   console.log("FlowForge server running on port 4000");
 });
